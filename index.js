de7e8d0b15202b1897c23127590c6851f0c7ba95
Afeez G Lawal <afeezlg@gmail.com>
2022-10-31 00:05:15 +0100

    Added cors middleware.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 const bio = {
   slackUsername: "Afeez",
