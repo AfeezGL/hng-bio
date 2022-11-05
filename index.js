04418420d28cf70fef5bb60c829a62d67560c528
Afeez G Lawal <afeezlg@gmail.com>
2022-11-05 08:07:13 +0100

    Added arithmetic operation post route.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
 app.use(cors());
 
 const bio = {
@@ -15,11 +17,36 @@ app.get('/', (req, res) => {
   res.json(bio);
 });
 
+app.post('/arithmetic-operation/', (req, res) => {
+  const allowedOperations = ['addition', 'subtraction', 'multiplication'];
+
+  if (
+    !allowedOperations.includes(req.body.operation_type) ||
+    isNaN(req.body.x) ||
+    isNaN(req.body.y)
+  )
+    return res.status(404).json({ error: 'Operation not supported.' });
+
+  let result;
+  if (req.body.operation_type === 'addition') {
+    result = req.body.x + req.body.y;
+  } else if (req.body.operation_type === 'subtraction') {
+    result = req.body.x - req.body.y;
+  } else {
+    result = req.body.x * req.body.y;
+  }
+
+  res.json({
+    slackUsername: bio.slackUsername,
+    result,
+    operation_type: req.body.operation_type,
+  });
+});
+
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 server.setTimeout(10000, (socket) => {
-  console.log('Timeout.');
   socket.destroy();
 });
